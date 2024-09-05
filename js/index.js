cde42683447a7ea5786c7427c2b590e39155a872
VertigoFromOuterSpace <justhenriquecarvalho@gmail.com>
2024-09-05 08:59:24 -0300

    atu-5/9

---
+++ js/index.js
@@ -0,0 +1,5 @@
+const diaSemana = document.getElementById("dia-semana");
+const diaMesAno = document.getElementById("dia-mes-ano");
+const horasMinSeg = document.getElementById("hora-min-seg");
+
+diaSemana.textContent = ;
