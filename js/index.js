0bd4bd9c075d6ad8213b7b6d81c2702a3b7b4bfd
VertigoFromOuterSpace <justhenriquecarvalho@gmail.com>
2024-09-05 09:25:16 -0300

    attu 5-9

--- js/index.js
+++ js/index.js
@@ -1,5 +1,30 @@
 const diaSemana = document.getElementById("dia-semana");
 const diaMesAno = document.getElementById("dia-mes-ano");
 const horasMinSeg = document.getElementById("hora-min-seg");
+const arrayWeekDay = ["Domingo","Segunda-feira", "Terça-feira","Quarta-feira","Quinta-feira","Sexta-feira","Sábado"];
 
-diaSemana.textContent = ;
+
+function daySemana(){
+    const date = new Date();
+    return arrayWeekDay[date.getDay()]
+}
+
+function dataCompleta(){
+    const date = new Date();
+    return date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
+}
+
+function horaCompleta(){
+    const date = new Date();
+    return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+}
+
+function atualizarHora(){
+    horasMinSeg.textContent = horaCompleta();
+}
+
+setInterval(atualizarHora, 1000);
+
+
+diaSemana.textContent = daySemana();
+diaMesAno.textContent = dataCompleta();
